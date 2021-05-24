0e70088219de06ef48a95b1b437f927305351ec1
JChoPop <62315952+JChoPop@users.noreply.github.com>
2021-05-24 17:55:09 -0400

    Add files via upload

---
+++ Test/test2_JC.py
@@ -0,0 +1,61 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+enA = 25
+in1 = 23
+in2 = 24
+
+enB = 22
+in3 = 17
+in4 = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+
+GPIO.setup(enA, GPIO.OUT)
+GPIO.setup(enB, GPIO.OUT)
+
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+
+p = GPIO.PWM(enA, 1000)
+p = GPIO.PWM(enB, 1000)
+p.start(25)
+speeds = [25, 50, 75]
+
+for values in speeds:
+    p.ChangeDutyCycle(values)
+
+    #forward
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.LOW)
+    sleep(3)
+
+    #stop
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    sleep(3)
+
+    #backward
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.HIGH)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
+    sleep(3)
+
+    #stop
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    sleep(3)
+GPIO.cleanup()
