9715524939b345a4124f049b84844a916ecf750f
JChoPop <62315952+JChoPop@users.noreply.github.com>
2021-05-25 12:05:55 -0400

    Add files via upload

--- Test/test2_JC.py
+++ Test/test2_JC.py
@@ -1,14 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
+#Motor A
 enA = 25
 in1 = 23
 in2 = 24
-
+#Motor B
 enB = 22
 in3 = 17
 in4 = 27
-
+#Setup as outputs
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
@@ -22,20 +23,27 @@ GPIO.output(in1, GPIO.LOW)
 GPIO.output(in2, GPIO.LOW)
 GPIO.output(in3, GPIO.LOW)
 GPIO.output(in4, GPIO.LOW)
-
-p = GPIO.PWM(enA, 1000)
-p = GPIO.PWM(enB, 1000)
-p.start(25)
+GPIO.output(enA, GPIO.HIGH)
+GPIO.output(enB, GPIO.HIGH)
+#Enable variable speeds through pwm
+pA = GPIO.PWM(enA, 1000)
+pB = GPIO.PWM(enB, 1000)
+pA.start(25)
+pB.start(25)
 speeds = [25, 50, 75]
 
+#Cycle through testing forward and backward at different speeds
 for values in speeds:
-    p.ChangeDutyCycle(values)
+    pA.ChangeDutyCycle(values)
+    pB.ChangeDutyCycle(values)
 
     #forward
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.HIGH)
     GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
     sleep(3)
 
     #stop
@@ -43,6 +51,8 @@ for values in speeds:
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    GPIO.output(enB, GPIO.LOW)
     sleep(3)
 
     #backward
@@ -50,6 +60,8 @@ for values in speeds:
     GPIO.output(in2, GPIO.HIGH)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.HIGH)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
     sleep(3)
 
     #stop
@@ -57,5 +69,8 @@ for values in speeds:
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    GPIO.output(enB, GPIO.LOW)
     sleep(3)
+
 GPIO.cleanup()
