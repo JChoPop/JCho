855cb1f0ebeef289cc1eac41097cccdb843ed288
JChoPop <62315952+JChoPop@users.noreply.github.com>
2021-05-25 13:48:34 -0400

    Add files via upload

--- Test/test2_JC.py
+++ Test/test2_JC.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-#Motor A
+# Motor A
 enA = 25
 in1 = 23
 in2 = 24
-#Motor B
+# Motor B
 enB = 22
 in3 = 17
 in4 = 27
-#Setup as outputs
+# Setup as outputs
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
@@ -25,19 +25,10 @@ GPIO.output(in3, GPIO.LOW)
 GPIO.output(in4, GPIO.LOW)
 GPIO.output(enA, GPIO.HIGH)
 GPIO.output(enB, GPIO.HIGH)
-#Enable variable speeds through pwm
-pA = GPIO.PWM(enA, 1000)
-pB = GPIO.PWM(enB, 1000)
-pA.start(25)
-pB.start(25)
-speeds = [25, 50, 75]
 
-#Cycle through testing forward and backward at different speeds
-for values in speeds:
-    pA.ChangeDutyCycle(values)
-    pB.ChangeDutyCycle(values)
 
-    #forward
+while (1):
+    # forward
     GPIO.output(in1, GPIO.HIGH)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.HIGH)
@@ -46,16 +37,16 @@ for values in speeds:
     GPIO.output(enB, GPIO.HIGH)
     sleep(3)
 
-    #stop
+    # stop
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
     GPIO.output(enA, GPIO.LOW)
     GPIO.output(enB, GPIO.LOW)
-    sleep(3)
+    sleep(1)
 
-    #backward
+    # backward
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.HIGH)
     GPIO.output(in3, GPIO.LOW)
@@ -64,13 +55,40 @@ for values in speeds:
     GPIO.output(enB, GPIO.HIGH)
     sleep(3)
 
-    #stop
+    # stop
     GPIO.output(in1, GPIO.LOW)
     GPIO.output(in2, GPIO.LOW)
     GPIO.output(in3, GPIO.LOW)
     GPIO.output(in4, GPIO.LOW)
     GPIO.output(enA, GPIO.LOW)
     GPIO.output(enB, GPIO.LOW)
+    sleep(1)
+
+    # steer right
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
+    sleep(3)
+
+    # stop
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    GPIO.output(enB, GPIO.LOW)
+    sleep(1)
+
+    # steer left
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.HIGH)
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
     sleep(3)
 
 GPIO.cleanup()
--- Test/test_JC.py
+++ Test/test_JC.py
@@ -1,23 +1,96 @@
-from gpiozero import Robot
+import RPi.GPIO as GPIO
 from time import sleep
 
-tote = Robot(left=(13, 15), right=(18, 20))
+# Motor A
+enA = 25
+in1 = 23
+in2 = 24
+# Motor B
+enB = 22
+in3 = 17
+in4 = 27
+# Setup as outputs
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+GPIO.setup(in3, GPIO.OUT)
+GPIO.setup(in4, GPIO.OUT)
+
+GPIO.setup(enA, GPIO.OUT)
+GPIO.setup(enB, GPIO.OUT)
+
+GPIO.output(in1, GPIO.LOW)
+GPIO.output(in2, GPIO.LOW)
+GPIO.output(in3, GPIO.LOW)
+GPIO.output(in4, GPIO.LOW)
+GPIO.output(enA, GPIO.HIGH)
+GPIO.output(enB, GPIO.HIGH)
 
 
-tote.forward(0.5, 3)
-tote.stop()
-tote.right(0.5, 2)
-tote.stop()
-tote.forward(0.5, 3)
-tote.stop()
-tote.left(0.5, 2)
-tote.stop()
-tote.backward(0.5, 3)
-tote.stop()
-tote.left(0.5, 2)
-tote.stop()
-tote.forward(0.5, 3)
-tote.stop()
+while (1):
+    # forward
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
+    sleep(3)
+
+    # stop
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    GPIO.output(enB, GPIO.LOW)
+    sleep(1)
+
+    # backward
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.HIGH)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
+    sleep(3)
+
+    # stop
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    GPIO.output(enB, GPIO.LOW)
+    sleep(1)
+
+    # steer right
+    GPIO.output(in1, GPIO.HIGH)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.HIGH)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
+    sleep(3)
+
+    # stop
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.LOW)
+    GPIO.output(in3, GPIO.LOW)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.LOW)
+    GPIO.output(enB, GPIO.LOW)
+    sleep(1)
+
+    # steer left
+    GPIO.output(in1, GPIO.LOW)
+    GPIO.output(in2, GPIO.HIGH)
+    GPIO.output(in3, GPIO.HIGH)
+    GPIO.output(in4, GPIO.LOW)
+    GPIO.output(enA, GPIO.HIGH)
+    GPIO.output(enB, GPIO.HIGH)
+    sleep(3)
+
 
 # La Croix Flavor Detector - Machine Learning on Raspberry Pi
 #
